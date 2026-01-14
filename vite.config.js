af1aca1cc0e7fe05c665492032aa4e42d7fb5279
aryanbatras <batraaryan03@gmail.com>
2026-01-14 14:53:52 +0530

    deploy7

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/js-hub/' : '/',
   plugins: [react()],
-  // Add this if you're using Three.js or other heavy libraries
-  build: {
-    chunkSizeWarningLimit: 1000, // in kbs
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  resolve: {
+    alias: {
+      // This will make '/assets' point to the public directory
+      '/assets': resolve(__dirname, 'public')
+    }
   }
-})
+});
