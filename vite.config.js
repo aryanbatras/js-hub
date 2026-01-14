682ab4dc627120c344808f8f43d694c058108169
aryanbatras <batraaryan03@gmail.com>
2026-01-14 14:40:03 +0530

    deploy3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/js-hub/' : '/',
   plugins: [react()],
+  // Add this if you're using Three.js or other heavy libraries
+  build: {
+    chunkSizeWarningLimit: 1000, // in kbs
+  }
 })
