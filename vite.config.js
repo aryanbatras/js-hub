f53c8de38c3f4fbe0c68f46d173e3834cabc3a2a
aryanbatras <batraaryan03@gmail.com>
2026-01-14 14:50:15 +0530

    deploy6

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/js-hub/' : '/',
   plugins: [react()],
+  // Add this if you're using Three.js or other heavy libraries
   build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          three: ['three', '@react-three/fiber', '@react-three/drei'],
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
-  },
-  server: {
-    fs: {
-      strict: false
-    }
+    chunkSizeWarningLimit: 1000, // in kbs
   }
 })
